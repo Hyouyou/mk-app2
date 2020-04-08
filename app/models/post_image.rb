c6abc15b11f5635518d945734e026af222c71bd2
Hyouyou <stbn1225@gmail.com>
2020-04-08 23:01:35 +0900

    [Finish]5-3

---
+++ app/models/post_image.rb
@@ -0,0 +1,6 @@
+class PostImage < ApplicationRecord
+
+  belongs_to :user
+  attachment :image#idは含めない
+
+end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '36c7db97e94b1b941f2f3555e0f4d934a40c61ee25d52eb4c4e27ad256fe0d581f9c5c8b208bfb1136239aaeb944cd653b8ffbd73dd49fb6e059badace3573f7'
